8de2f685e1149b6152839f4ce11af546b25518d5
bj0ris <btthunem@gmx.com>
2017-11-24 10:43:24 +0100

    Finsihed collision detect between asteroids, they do however tend to get stuck in each other

--- classes/coordsClass.js
+++ classes/coordsClass.js
@@ -4,8 +4,12 @@ class Coords{
     constructor(_x,_y){
         this.x = _x;
         this.y = _y;
+        this.prevX;
+        this.prevY;
     }
     redefineOrigoTo(_x,_y){
+        this.prevX = this.x;
+        this.prevY = this.y;
         this.x -= _x;
         this.y -= _y;
     }
